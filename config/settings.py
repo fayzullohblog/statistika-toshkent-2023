cff0f429fa783aa3d1207c75bd7fd8800b81231a
portfolio1 <fayzulloh00010002@gmail.com>
2023-07-03 15:28:46 +0500

    a

--- config/settings.py
+++ config/settings.py
@@ -146,9 +146,9 @@ import os
 STATIC_URL = 'static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # STATIC_ROOT=BASE_DIR / 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static/'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static/'
+# ]
 # STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
